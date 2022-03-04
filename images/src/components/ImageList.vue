a70a601cf662b1c4da928fd68dc2cc8b7c3ee464
Jung Park <jungsun.park@relativity.com>
2022-03-04 16:32:21 -0600

    fetch and display images

--- images/src/components/ImageList.vue
+++ images/src/components/ImageList.vue
@@ -1,9 +1,18 @@
 <template>
-	<div>Image List</div>
+	<div>
+		<img v-for="image in allImages" :key="image.id" :src="image.link" />
+	</div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
+
 export default {
-	name: 'ImageList'
+	name: 'ImageList',
+	methods: mapActions(['fetchImages']),
+	computed: mapGetters(['allImages']),
+	created() {
+		this.fetchImages();
+	}
 }
 </script>
